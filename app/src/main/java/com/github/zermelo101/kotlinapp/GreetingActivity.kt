7104790925ca1df788d86433c964378c196a3b5c
Jean-Francois/Zermelo <jean-francois.rocher@epfl.ch>
2023-03-01 12:31:14 +0100

    try fixing CI

--- app/src/main/java/com/github/zermelo101/kotlinapp/GreetingActivity.kt
+++ app/src/main/java/com/github/zermelo101/kotlinapp/GreetingActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class GreetingActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class GreetingActivity : AppCompatActivity() {
         val receivedIntent : Intent = this.intent
         val name : String? = receivedIntent.getStringExtra("name")
         setContentView(R.layout.activity_greeting)
-        val text : EditText = findViewById<EditText>(R.id.GreetingName)
-        text.setText("Welcome " + name)
+        val text : TextView = findViewById<EditText>(R.id.GreetingName)
+        text.text = "Welcome $name"
 
 
     }
