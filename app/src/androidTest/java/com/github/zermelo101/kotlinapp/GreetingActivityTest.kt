c4a19484f49b255e9e564b5e975da83397436385
Jean-Francois/Zermelo <jean-francois.rocher@epfl.ch>
2023-03-01 11:46:53 +0100

    try fixing CI

--- app/src/androidTest/java/com/github/zermelo101/kotlinapp/GreetingActivityTest.kt
+++ app/src/androidTest/java/com/github/zermelo101/kotlinapp/GreetingActivityTest.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
