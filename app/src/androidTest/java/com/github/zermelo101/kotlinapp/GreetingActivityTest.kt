2f07992f028748778a31855bdddc7d80b327fafd
Jean-Francois/Zermelo <jean-francois.rocher@epfl.ch>
2023-03-01 12:00:07 +0100

    try fixing CI

--- app/src/androidTest/java/com/github/zermelo101/kotlinapp/GreetingActivityTest.kt
+++ app/src/androidTest/java/com/github/zermelo101/kotlinapp/GreetingActivityTest.kt
@@ -1,5 +1,4 @@
 package com.github.zermelo101.kotlinapp
-
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
@@ -7,17 +6,22 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class GreetingActivityTest {
+class GreetingActivityTest{
+
     @Test
-    fun testGreetingActivity(){
-        val intent = Intent(ApplicationProvider.getApplicationContext(),GreetingActivity::class.java)
-        intent.putExtra("name","Jeff")
-        val x : ActivityScenario<GreetingActivity> = ActivityScenario.launch(intent)
-        onView(ViewMatchers.withText("Welcome Jeff")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        x.close()
+    fun greeting(){
+        val greeting = Intent(ApplicationProvider.getApplicationContext(), GreetingActivity::class.java)
+        val name = "Steve"
+        greeting.putExtra("name",name)
+
+        var res : ActivityScenario<GreetingActivity> = ActivityScenario.launch(greeting)
+        onView(ViewMatchers.withText("Hello Steve")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        res.close()
     }
+
 }
